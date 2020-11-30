27022d120aadd654b5cf4980a723f47d98f3c6d1
pietropezzi <pietro.pezzi3@studio.unibo.it>
2020-11-30 11:01:31 +0100

    Added es3_macroALLOCAVETTORE in C same ex as es2_ALLOCAVETTORE, without macro.h

---
+++ C/Lesson6/es3_macroALLOCAVETTORE/README.md
@@ -0,0 +1,2 @@
+*To compile main.c use:*
+gcc -ansi -Wpedantic -Wall  -c -D'ALLOCAVETTORE(PTR)=do { PTR=(int32_t*)malloc(10*sizeof(int32_t)); if(PTR!=NULL) { int j; for(j=0;j<10;j++) {PTR[j]=-1000+j; } } } while(0)' main.c
---
+++ C/Lesson6/es3_macroALLOCAVETTORE/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int main(void){
+	int32_t *p;
+	int check=0,j=0,i=0;
+	ALLOCAVETTORE(p);
+	for(i=-1000;i<=-991;i++){
+		if(p[j]==i){
+			check++;
+		}
+		j++;
+	}
+
+	if(check==10){
+		printf("the macro added the correct values!\n");
+	}
+		
+	for(i=0;i<10;i++)
+		p[i]=-19+i;
+
+	for(i=0;i<10;i++){
+		printf("%d ,",p[i]);
+		fflush(stdout);
+	}
+	printf("\n");
+	 
+	return(0);
+}
