72fc9e978e69aaa066e837b1f8566a0365978bae
pietropezzi <pietro.pezzi3@studio.unibo.it>
2020-11-30 16:19:09 +0100

    Completed Lesson6 in both scripts and C Added last Lesson6 exercise the "ESERCIZI_DA_CORREGGERE_ERRORI_1" ex All sub dirs of the 1 dir contain all the corrected versions.

---
+++ C/Lesson6/ES3/1/1.6/main.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "def.h"
+#include "A.h"
+
+int main(void){ 
+	SD A={33.666}, B; 
+
+	B = fA( &A );
+	printf("%f\n", B.x );
+        if( B.x > 1.1 )
+                return( 1 );
+        else
+                return( 0 );
+}
+
+
